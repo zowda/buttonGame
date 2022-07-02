ab235db1e70f701f80ecced91fdd3c62d941a30c
Zaher <zowda@users.noreply.github.com>
2022-07-02 14:31:45 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let X = 0
+let Y = 0
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    led.toggle(X, Y)
+    if (input.buttonIsPressed(Button.A)) {
+        Y += 1
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else {
+        if (input.buttonIsPressed(Button.B)) {
+            X += 1
+            music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        }
+    }
+})
